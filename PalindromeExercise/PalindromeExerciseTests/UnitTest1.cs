bea3aa6c46ddb28f62c451641fe2f1d87ff732d9
kaylafinkelstein <k.finkelstein4000@gmail.com>
2022-06-09 11:49:55 -0400

    PalindromeExercise

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -5,10 +5,17 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hello", false)]
+      
+        public void IsAPalindrome(string word, bool expected)
         {
+            var tester = new WordSmith();
 
+            var actual = tester.IsAPalindrome(word);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
---
+++ PalindromeExercise/PalindromeExerciseTests/WordSmith.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PalindromeExerciseTests
+{
+    public class WordSmith
+    {
+
+        public bool IsAPalindrome(string word)
+        {
+            char[] charsArray = word.ToCharArray();
+            Array.Reverse(charsArray);
+            var reversed = new string(charsArray);
+
+            if (reversed == word)
+            {
+                return true;
+            }
+            else
+            {
+                return false; 
+            }
+        }
+    }
+}
